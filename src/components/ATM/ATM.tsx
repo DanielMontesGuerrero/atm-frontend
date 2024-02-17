4a01321f08a788f6be28a95ee54307d887383e52
DanielMontesGuerrero <danimontesguerrero17@gmail.com>
2024-02-17 15:46:02 -0600

    Add ATM component

---
+++ src/components/ATM/ATM.tsx
@@ -0,0 +1,18 @@
+import "./ATM.css";
+import CardsBox from "./CardsBox";
+import Screen from "./Screen";
+import sticker from "../../assets/sticker_graf.png";
+
+const ATM = () => {
+  return (
+    <div className="atm">
+      <CardsBox/>
+      <Screen/>
+      <div className="sticker-logo">
+        <img src={sticker} alt="sticker" />
+      </div>
+    </div>
+  );
+};
+
+export default ATM;
