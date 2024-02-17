0adcb94ed0ff9368ec7d91c9b68100733f6a73b4
DanielMontesGuerrero <danimontesguerrero17@gmail.com>
2024-02-17 15:40:34 -0600

    Add Banner component

---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,18 @@
+import atm from '../../assets/atm_sign.png';
+import graffiti from '../../assets/graffiti.png';
+import './Banner.css';
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <div className="atm-logo">
+        <img src={atm} alt="ATM logo" />
+      </div>
+      <div className="graffiti-logo">
+        <img src={graffiti} alt="graffiti logo" />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
